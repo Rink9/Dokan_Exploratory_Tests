9fa93d8191fd03c62d296f5971cd3b2a125ee933
Rinky Chowdhury <lutfunrinky.129@gmail.com>
2021-11-30 13:59:49 +0600

    Updated configuration file

--- codecept.conf.js
+++ codecept.conf.js
@@ -61,51 +61,17 @@ exports.config = {
                 admin: {
                     login: (I) => {
                         I.amOnPage('/wp-admin/');
-                        I.fillField('#user_login', env('ADMIN'));
-                        I.fillField('#user_pass', env(secret('ADMIN_PASSWORD')));
+                        I.fillField('#user_login', 'username');
+                        I.fillField('#user_pass', 'password');
                         I.click('Log In');
                     },
                     check: (I) => {
                         I.seeCurrentUrlEquals('/wp-admin/');
                     },
                 },
-                Vendor: {
-                    login: (I) => {
-                        I.amOnPage('/my-account/');
-                        I.fillField('username', env('VENDOR_ONE'));
-                        I.fillField('password', env(secret('VENDOR_ONE_PASSWORD')));
-                        I.click('login');
-                    },
-                },
-                VendorTwo: {
-                    login: (I) => {
-                        I.amOnPage('/my-account/');
-                        I.fillField('username', env('VENDOR_TWO'));
-                        I.fillField('password', env(secret('VENDOR_TWO_PASSWORD')));
-                        I.click('login');
-                    },
-                },
-                Customer: {
-                    login: (I) => {
-                        I.amOnPage('/my-account/');
-                        I.fillField('username', env('CUSTOMER_ONE'));
-                        I.fillField('password', env(secret('CUSTOMER_ONE_PASSWORD')));
-                        I.click('login');
-                    }
-                },
-                CustomerTwo: {
-                    login: (I) => {
-                        I.amOnPage('/my-account/');
-                        I.fillField('username', env('CUSTOMER_TWO'));
-                        I.fillField('password', env(secret('CUSTOMER_TWO_PASSWORD')));
-                        I.click('login');
-                    }
-                }
+                
             }
         },
     }
 
-    // tests:'core-tests/**/*_test.js',
-    // // tests: './core-tests/*_test.js',
-    // name: 'e2e'
 }
--- env.js
+++
@@ -1,3 +0,0 @@
-module.exports= function(envVariable){
-    return process.env[envVariable]
-}
